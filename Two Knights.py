251addc64dca8d7bf4cacaf3d0832787bd0d4098
krishnaj-13 <111274796+krishnaj-13@users.noreply.github.com>
2025-08-13 11:07:01 +0530

    Add files via upload

---
+++ Two Knights.py
@@ -0,0 +1,9 @@
+a = int(input())
+for i in range (1,a+1,1):
+    total = 0
+    total = int(((i*i)*((i*i) -1))/2)
+    a = i - 2
+    b = i - 1 
+    attack = a * b * 4
+    print(total-attack)
+    
